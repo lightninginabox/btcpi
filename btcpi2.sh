c538a234d52511be413e24eb09f119eabab9c9c7
lightninginabox <92657862+lightninginabox@users.noreply.github.com>
2021-10-27 23:25:51 -0400

    Update btcpi2.sh

--- btcpi2.sh
+++ btcpi2.sh
@@ -9,8 +9,12 @@ export BTCPAYGEN_REVERSEPROXY="nginx"
 export BTCPAYGEN_ADDITIONAL_FRAGMENTS="opt-more-memory;opt-save-storage-s"
 export BTCPAY_ENABLE_SSH=true
 
-isSDMounted=$(sudo df | grep -c /dev/mmcblk0p1)
-if [ ${isSDMounted} -eq 1 ]; then
+isSD=$(fdisk -l | grep -c "/dev/mmcblk0:")
+isNVMe=$(fdisk -l | grep -c "/dev/nvme0n1:")
+isUSB=$(fdisk -l | grep -c "/dev/sda:")
+
+# When booting from SD card with attached USB drive
+if [ ${isSD} -eq 1 ] && [ ${isUSB} -eq 1 ]; then
   mkdir -p /mnt/usb
   sfdisk --delete /dev/sda
   sleep 5
@@ -19,12 +23,31 @@ if [ ${isSDMounted} -eq 1 ]; then
   parted /dev/sda mkpart primary ext4 0% 100%
   sleep 5
   yes | mkfs.ext4 /dev/sda1
+  sleep 10
   UUID="$(sudo blkid -s UUID -o value /dev/sda1)"
   echo "UUID=$UUID /mnt/usb ext4 defaults,noatime,nofail 0 0" | tee -a /etc/fstab
   mount /dev/sda1 /mnt/usb
   sleep 5
   mkdir -p /mnt/usb/docker
   ln -s /mnt/usb/docker /var/lib/docker
+fi
+# When booting from SD card with NVMe drive attached
+if [ ${isSD} -eq 1 ] && [ ${isNVMe} -eq 1 ]; then
+  mkdir -p /mnt/nvme
+  sfdisk --delete /dev/nvme0n1
+  sleep 5
+  parted -s -a optimal /dev/nvme0n1 mklabel gpt
+  sleep 5
+  parted -s -a optimal /dev/nvme0n1 mkpart primary ext4 0% 100%
+  sleep 5
+  yes | mkfs.ext4 /dev/nvme0n1p1
+  sleep 10
+  UUID="$(sudo blkid -s UUID -o value /dev/nvme0n1p1)"
+  echo "UUID=$UUID /mnt/nvme ext4 defaults,noatime,nofail 0 0" | tee -a /etc/fstab
+  mount /dev/nvme0n1p1 /mnt/nvme
+  sleep 5
+  mkdir -p /mnt/nvme/docker
+  ln -s /mnt/nvme/docker /var/lib/docker
 fi
 
 dphys-swapfile swapoff
